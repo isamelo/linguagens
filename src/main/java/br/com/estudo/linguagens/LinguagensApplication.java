51dce02a3d499cf6d44f8584e16fff9a1ac12575
isamelo <isars.melo@gmail.com>
2022-07-25 22:23:01 -0300

    Criação base projeto

---
+++ src/main/java/br/com/estudo/linguagens/LinguagensApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudo.linguagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinguagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinguagensApplication.class, args);
+	}
+
+}
